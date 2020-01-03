e30dd0334cb23233bc3ad9c82c2180e815bb77c7
jerb23 <jjerb23_@hotmail.com>
2020-01-03 11:13:08 +0000

    React Node and MongoDB app
    Aplication react, node, MongoDb

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const productSchema = new Schema({
+    name: {type: String},
+    description: {type: String},
+})
+
+module.exports =  mongoose.model('products', productSchema);
